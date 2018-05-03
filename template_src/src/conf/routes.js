6278c4508fb487460d3eca38c10e054b09a5555f
mhf <mhfcbz@163.com>
2018-05-03 10:52:02 +0800

    s:

--- template_src/src/conf/routes.js
+++ template_src/src/conf/routes.js
@@ -2,12 +2,9 @@
 import NotFound from "../root/NotFound.vue"
 
 import Main from "../root/main/a.vue"
-import File from "../root/file/a.vue"
 
 export default [
   { path: "/", component: Main },
-
-  { path: "/file", component: File },
 
   // cordova
   { path: "/index.html", component: Main },
